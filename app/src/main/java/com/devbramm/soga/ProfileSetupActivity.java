6587d0c6bc711411e85ab5ccbf90d16725ef5792
DevBramm <bramwellokoth@gmail.com>
2023-10-15 12:09:33 +0300

    Profile Setup Screen

--- app/src/main/java/com/devbramm/soga/ProfileSetupActivity.java
+++ app/src/main/java/com/devbramm/soga/ProfileSetupActivity.java
@@ -2,7 +2,9 @@ package com.devbramm.soga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileSetupActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ProfileSetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
+
+        findViewById(R.id.finish_setup_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileSetupActivity.this, MainActivity.class));
+            }
+        });
     }
 }
